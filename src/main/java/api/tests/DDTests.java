2d03e0806f73995229becfd16ea1b071613bff0d
bahodurotayev <tjkbaha@gmail.com>
2022-12-26 07:00:13 -0500

    Utils

--- src/main/java/api/tests/DDTests.java
+++ src/main/java/api/tests/DDTests.java
@@ -1,11 +1,25 @@
 package api.tests;
 
+import api.endpoints.UserEndPoints;
+import api.payloads.User;
 import api.utils.DataProviders;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.testng.annotations.Test;
 
 public class DDTests {
     @Test(priority = 1, dataProvider = "Data", dataProviderClass = DataProviders.class)
     public void testPostUser(String userID, String username, String fName, String lName, String email, String pwd, String phone){
+        User userPayload = new User();
+        userPayload.setId(Integer.parseInt(userID));
+        userPayload.setUsername(username);
+        userPayload.setFirstName(fName);
+        userPayload.setLastName(lName);
+        userPayload.setEmail(email);
+        userPayload.setPassword(pwd);
+        userPayload.setPhone(phone);
 
+        Response response = UserEndPoints.createUser(userPayload);
+        Assert.assertEquals(response.getStatusCode(), 200);
     }
 }
